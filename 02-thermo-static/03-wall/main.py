977f376e4016dee3b20400588fe9957672a6cdc8
Jan Friederich <jan.friederich@gmx.de>
2017-10-25 13:05:00 +0200

    add thermo-static

---
+++ 02-thermo-static/03-wall/main.py
@@ -0,0 +1,72 @@
+from dolfin import *
+
+################################
+#### PROBLEM DEFINITION ########
+################################
+
+# Define geometry and mesh
+d = 3
+p0 = Point(0.0, 0.0, 0.0)
+p1 = Point(30.0, 10.0, 10.0)
+mesh = BoxMesh(p0, p1, 30, 10, 10)
+
+# Define boundaries
+boundaries = FacetFunction("size_t", mesh)
+boundaries.set_all(0)
+left, right, bottom, top = 1, 2, 3, 4
+CompiledSubDomain("near(x[0], side) && on_boundary", side = p0[0]).mark(boundaries, left)
+CompiledSubDomain("near(x[0], side) && on_boundary", side = p1[0]).mark(boundaries, right)
+CompiledSubDomain("near(x[1], side) && on_boundary", side = p0[1]).mark(boundaries, bottom)
+CompiledSubDomain("near(x[1], side) && on_boundary", side = p1[1]).mark(boundaries, top)
+
+# Coordinate and surface integral element
+x = SpatialCoordinate(mesh)
+ds = Measure('ds', domain=mesh, subdomain_data=boundaries)
+
+# Define function space
+p = 2
+V = FunctionSpace(mesh, "Lagrange", p)
+
+# Define trial and test functions
+theta = TrialFunction(V)
+delta_theta = TestFunction(V)
+
+# Material parameters
+kappa = 1.0*conditional(lt(x[0], p1[0]/3.0), 2.0, conditional(lt(x[0], p1[0]*2.0/3.0), 0.3, 1.0))
+
+# Volume force/ heat source and prescribed tractions/ prescribed heat fluxes
+r = Constant(0.0)
+q_p = Constant(0.0)
+
+# Dirichlet boundary conditions
+bcs = [DirichletBC(V, Constant((25.0)), boundaries, left),
+	   DirichletBC(V, Constant((-10.0)), boundaries, right)]
+
+# Weak form a==l
+a = kappa*dot(grad(delta_theta), grad(theta))*dx
+l = r*delta_theta*dx + q_p*delta_theta*ds(top)
+
+################################
+#### ASSEMBLE AND SOLVE ########
+################################
+
+
+theta = Function(V)
+
+A = assemble(a)
+B = assemble(l)
+for bc in bcs:
+	bc.apply(A, B)
+X = theta.vector()
+solve(A, X, B)
+
+
+################################
+#### POST-PROCESSING ###########
+################################
+
+# Create temperature file
+theta.rename("theta", "temperature")
+file_theta = File("temperature.pvd", "compressed")
+file_theta << theta
+
