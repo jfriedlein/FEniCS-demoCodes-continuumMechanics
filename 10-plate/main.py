635805246e10ca4cc28cd6840ee5374ff68f9a6e
Jan Friederich <jan.friederich@ltm.uni-erlangen.de>
2018-02-03 19:04:11 +0100

    final version plate

--- 10-plate/main.py
+++ 10-plate/main.py
@@ -2,7 +2,6 @@ from fenics import *
 from mshr import *
 import numpy as np
 
-
 set_log_level(WARNING)
 
 class Stress(Expression):
@@ -54,13 +53,65 @@ class Stress(Expression):
         values[2] = values[1]
     
 
+cpp_code = '''
+class Stress : public Expression {
+
+public:
+    Stress() : Expression(2, 2), a(1.0), b(1.0), beta(0.0), t(1.0) {}
+        
+    void eval(Array<double> &values, const Array<double> &x) const {
+        const double m = (a-b)/(a+b);
+        
+        double signx = (x[0] > 0) ? 1 : ((x[0] < 0) ? -1 : 0);
+        double signy = (x[1] > 0) ? 1 : ((x[1] < 0) ? -1 : 0);
+        
+        double ure = (x[0]*x[0]-x[1]*x[1])-(a*a-b*b);
+        double urho = sqrt(ure*ure+4.0*x[0]*x[0]*x[1]*x[1]);
+        double zetare = (x[0]+signx*sqrt(0.5*max(urho+ure, 0.0)))/(a+b);
+        double zetaim = (x[1]+signy*sqrt(0.5*max(urho-ure, 0.0)))/(a+b);
+        double rho = sqrt(zetare*zetare+zetaim*zetaim);
+        double theta = atan2(zetaim, zetare);
+        
+        double rho2 = rho*rho;
+        double rho4 = rho2*rho2;
+        double rho6 = rho4*rho2;
+        
+        double S1 = (rho4-2.0*rho2*cos(2.0*theta-2.0*beta)+2.0*m*cos(2.0*beta)-m*m)/(rho4-2.0*m*rho2*cos(2.0*theta)+m*m);
+	    
+        double S2zr = (rho6*cos(-6.0*theta+2.0*beta) - 2.0*m*rho2*(cos(-4.0*theta-2.0*beta) - m*cos(-4.0*theta)) - 3.0*m*rho4*cos(-4.0*theta+2.0*beta) - (m*m-2.0*cos(2.0*beta)*m+1.0)*rho4*cos(4.0*theta)	- 2.0*rho4*(cos(-2.0*theta-2.0*beta) - m*cos(-2.0*theta)) + 3.0*rho2*cos(2.0*theta+2.0*beta) - m*(m*m-2.0*cos(2.0*beta)*m+1.0)*rho2*cos(2.0*theta) - m*cos(2.0*beta));
+        double S2zi = (rho6*sin(-6.0*theta+2.0*beta) - 2.0*m*rho2*(sin(-4.0*theta-2.0*beta) - m*sin(-4.0*theta)) - 3.0*m*rho4*sin(-4.0*theta+2.0*beta) - (m*m-2.0*cos(2.0*beta)*m+1.0)*rho4*sin(-4.0*theta) - 2.0*rho4*(sin(-2.0*theta-2.0*beta) - m*sin(-2.0*theta)) + 3.0*rho2*sin(-2.0*theta-2.0*beta) - m*(m*m-2.0*cos(2.0*beta)*m+1.0)*rho2*sin(-2.0*theta) - m*sin(-2.0*beta));
+        
+        double S2nr = rho6*cos(6.0*theta)-3.0*m*rho4*cos(4.0*theta)+3.0*m*m*rho2*cos(2.0*theta)-m*m*m;
+        double S2ni = -rho6*sin(6.0*theta)+3.0*m*rho4*sin(4.0*theta)-3.0*m*m*rho2*sin(2.0*theta);
+        
+        double ReS2 = (S2zr*S2nr+S2zi*S2ni)/(S2nr*S2nr+S2ni*S2ni);
+        double ImS2 = (S2zi*S2nr-S2zr*S2ni)/(S2nr*S2nr+S2ni*S2ni);
+        
+        double sigmaxx = t*0.5*(S1+ReS2);
+        double sigmayy = t*0.5*(S1-ReS2);
+        double sigmaxy = t*0.5*(ImS2);
+        
+        //sigma_xx
+        values[0] = cos(beta)*cos(beta)*sigmaxx+sin(beta)*sin(beta)*sigmayy+2.0*cos(beta)*sin(beta)*sigmaxy;
+        //sigma_yy
+        values[3] = sin(beta)*sin(beta)*sigmaxx+cos(beta)*cos(beta)*sigmayy-2.0*cos(beta)*sin(beta)*sigmaxy;
+        //sigma_xy
+        values[1] = -cos(beta)*sin(beta)*sigmaxx+sin(beta)*cos(beta)*sigmayy+(cos(beta)*cos(beta)-sin(beta)*sin(beta))*sigmaxy;
+        //sigma_yx
+        values[2] = values[1];
+    }
+
+public:
+    double a, b, beta, t;
+}; 
+'''
 
 ################################
 #### PROBLEM DEFINITION ########
 ################################
 
 d = 2
-p = 3
+p = 1
 
 # Parameters
 A = 1
@@ -76,13 +127,17 @@ file_u = File("displacement.pvd", "compressed")
 file_s = File("stress.pvd", "compressed")
 
 file_h = open("history{}.dat".format(p), "w")
-file_h.write("elems\thmax\terror\tclock\n")
+file_h.write("elems\thmax\terror\tclock_mesh\tclock_solve\tclock_error\n")
 
-for i in range(1, 7):
+for i in range(2, 10):
     
+    print("MESH", i)
+    print("Generate: ", end="")
     # Create mesh
+    tic()
     mesh = generate_mesh(geometry, 2**i)
-    print("MESH {0} with n_elems={1} and h_max={2}".format(i, mesh.num_cells(), mesh.hmax()))
+    clock_mesh = toc()
+    print("n_elems={}, h_max={} ({})".format(mesh.num_cells(), mesh.hmax(), clock_mesh), flush=True)
     
     #mesh = refine(mesh)
 
@@ -134,7 +189,9 @@ for i in range(1, 7):
 
     # Volume force/ heat source and prescribed tractions/ prescribed heat fluxes
     b = Constant((0.0, 0.0))
-    stress = Stress(A, B, degree=p+3)
+    #stress = Stress(A, B, degree=p+2)
+    stress = Expression(cpp_code, degree=p+2)
+    stress.a, stress.b = 1.0, 1.0
     t_p_left = as_vector((-stress[0,0], -stress[0,1]))
     t_p_top = as_vector((stress[1,0], stress[1,1]))
 
@@ -151,16 +208,13 @@ for i in range(1, 7):
     a = inner(grad(delta_u), sigma(u))*dx
     l = dot(b, delta_u)*dx + dot(t_p_left, delta_u)*ds(left) + dot(t_p_top, delta_u)*ds(top)
 
-    #self.m = 0.5*inner(grad(u), sigma(u))*dx - dot(b, u)*dx - dot(t_p, u)*ds(right)
-
     ################################
     #### ASSEMBLE AND SOLVE ########
     ################################
 
+    print("Solve... ", end="")
     tic()
-
     u = Function(V)
-
     #K = assemble(a)
     #F = assemble(l)
     #for bc in bcs:
@@ -172,23 +226,40 @@ for i in range(1, 7):
     solve(a == l, u, bcs=bcs, 
 	      solver_parameters={"linear_solver": "mumps"},
 	      form_compiler_parameters={"optimize": True})
+    clock_solve = toc()
+    print("({})".format(clock_solve), flush=True)
+    
+    print("Error: ", end="")
+    tic()
+    e = stress-sigma(u)
+    error = sqrt(abs(assemble(inner(e,e)*dx)))    
 
-    clock = toc()
+    #E = TensorFunctionSpace(mesh, "Discontinuous Lagrange", p+2)
+    #stressE = interpolate(stress, E)
+    #sigmaE = project(sigma(u), E)
+    #eE = Function(E)
+    #eE.assign(stressE)
+    #eE.vector().axpy(-1.0, sigmaE.vector())
 
-    #e = as_matrix(((stress[0],stress[2]),(stress[2],stress[1])))-sigma(u)
-    #error = sqrt(abs(assemble(inner(e,e)*dx)))    
-    E = TensorFunctionSpace(mesh, "Discontinuous Lagrange", p+3)
-    stressE = interpolate(stress, E)
-    sigmaE = project(sigma(u), E)
-    eE = Function(E)
-    eE.assign(stressE)
-    eE.vector().axpy(-1.0, sigmaE.vector())
-    error = norm(eE, norm_type="L2", mesh=mesh)
+    #tic()
+    #stressE = interpolate(stress, E)
+    #print("\nintp",toc())
+    #tic()
+    #sigmaE = Function(E)
+    #sigmaE = project(sigma(u), E, solver_type="cg")
+    #print("proj",toc())
+    #tic()
+    #eE = Function(E)
+    #eE.assign(stressE)
+    #eE.vector().axpy(-1.0, sigmaE.vector())
+    #print("assg",toc())
+    #tic()
+    #error = norm(eE, norm_type="L2", mesh=mesh)
+    #print("norm",toc())
     
-    
-    print("ERROR:", error)
-    print("CLOCK:",clock,"\n")
-    
+    clock_error = toc()
+    print("{} ({})".format(error, clock_error), flush=True)
+        
     #errornorm(Expression((("0.0", "0.0"),("0.0", "0.0")), element=V.ufl_element()), sigma(u))
 
     ################################
@@ -200,23 +271,20 @@ for i in range(1, 7):
     file_u << u
 
     # Project stress field and create stress file
-    def dev(s):
-	    return s-tr(s)*Identity(d)/3.0
-    def von_mises(s):
-	    return sqrt(3.0/2.0*inner(dev(s), dev(s)))
+    #def dev(s):
+	#    return s-tr(s)*Identity(d)/3.0
+    #def von_mises(s):
+	#    return sqrt(3.0/2.0*inner(dev(s), dev(s)))
     S = FunctionSpace(mesh, "Lagrange", p)
-    T = TensorFunctionSpace(mesh, "Lagrange", p)
+    #T = TensorFunctionSpace(mesh, "Lagrange", p)
 
-    #stress = project(sigma(u)[0,0]/1.e6, S)
-    #stress.rename("stress", "xx")
-    #stress = project(von_mises(sigma(u))/1.e6, S)
-    #stress.rename("stress", "vonMises")
-    stress_proj = project(sigma(u)/1.e6, T, solver_type="mumps")
-    stress_proj.rename("sigma", "stress")
+    stress_proj = project(sigma(u)[0,0], S, solver_type="mumps")
+    stress_proj.rename("sigma_xx", "stress")
 
     file_s << stress_proj
 
     
-    file_h.write("{}\t{}\t{}\t{}\n".format(mesh.num_cells(), mesh.hmax(), error, clock))
+    file_h.write("{}\t{}\t{}\t{}\t{}\t{}\n".format(mesh.num_cells(), mesh.hmax(), error, clock_mesh, clock_solve, clock_error))
     
 file_h.close()
+
