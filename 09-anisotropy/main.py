978fbc17b92e3dda1f44536e18f6ed38b5445bd4
jfriedlein <58786257+jfriedlein@users.noreply.github.com>
2026-05-28 11:00:16 +0200

    some cleaning

--- 09-anisotropy/main.py
+++ 09-anisotropy/main.py
@@ -16,9 +16,12 @@ Geometry & Mesh:
 
 Material Model:
 - Transversely Isotropic Linear Elasticity:
-    * Longitudinal Young's Modulus (E_L): 44 GPa (Fiber direction)
-    * Transverse Young's Modulus (E_T): 13 GPa
-    * Fiber Direction (a1): Oriented along the Y-axis (0.0, 1.0, 0.0)
+    * Longitudinal Young's modulus (E_L): 44 GPa (Fiber direction)
+    * Transverse Young's modulus (E_T): 13 GPa
+	* Shear modulus in planes parallel to the fibres (mu_LT): 5.6 GPa
+	* Shear modulus in isotropic plane (mu_TT): 5.0 GPa
+	* Poisson's ratio for tension in fibre direction (nu_LT): 0.25
+    * Fibre direction (a1): Oriented e.g. along the Y-axis (0.0, 1.0, 0.0)
 
 Boundary Conditions:
 - Dirichlet BCs (Mixed Support):
@@ -82,46 +85,34 @@ delta_u = TestFunction(V) # test function for virtual displacement field
 #--------------------------------------------------------------------------------------------------------
 
 # Material parameters
-E_L = 44000*1.e6  #=E_11  # Young's modulus in longitudinal direction in Pa
-E_T = 13000*1.e6  #=E_22=E_33  # Young's modulus in transverse directions in Pa
+E_L = 44*1.e9  #=E_11  # Young's modulus in longitudinal direction in Pa
+E_T = 13*1.e9  #=E_22=E_33  # Young's modulus in transverse directions in Pa
+mu_LT = 5.6*1.e9 #=G_12=G_13 # Longitudinal shear modulus in Pa
+mu_TT = 5*1.e9 # =E_T/(2*(1+nu_TT)) #=G_23 # shear modulus in isotropic plane in Pa
 nu_LT = 0.25 #=nu=nu_12=nu_13 # Poisson's ratio for strain in longitudinal direction when stretched in transverse direction
-nu_TT = 0.3  #=nu_23=nu_32 # Poisson's ratio in transverse plane 
-mu_LT = 5600*1.e6 #=G_12=G_13 # Longitudinal shear modulus in Pa
-mu_TT = E_T/(2*(1+nu_TT)) #=G_23 # Transverse shear modulus in Pa
+nu_TT = (E_T/2 - mu_TT)/mu_TT # =0.3  #=nu_23=nu_32 # Poisson's ratio in transverse plane 
 nu_TL = E_T/E_L*nu_LT # Poisson's ratio for strain in transverse direction when stretched in longitudinal direction
 
 lmbda = (nu_LT*nu_TL+nu_TT)/(1-nu_TT-2*nu_LT*nu_TL)/(1+nu_TT)*E_T # Modified Lamé parameter for anisotropic material # Used in the stress-strain relationship
 alpha = (nu_LT*(1+nu_TT-nu_TL)-nu_TT)/(1-nu_TT-2*nu_LT*nu_TL)/(1+nu_TT)*E_T # Coupling parameter controlling interaction between longitudinal and transverse strains
 beta = E_L*(1-nu_TT*nu_TT)/(1-nu_TT-2*nu_LT*nu_TL)/(1+nu_TT)-lmbda-4*mu_LT # Additional stiffness parameter required for transversely isotropic elasticity
 
+# fibre direction
+# along e_1 (angle alpha=0°):
 #a1 = Constant((1.0, 0.0, 0.0))
-a1 = Constant((0.0, 1.0, 0.0)) # Fiber direction along the y-axis (longitudinal direction)
-
-# alternate fiber direction along 45 degree in x-y plane
+# along e_2 (angle alpha=90°):
+a1 = Constant((0.0, 1.0, 0.0)) # Fibre direction along the y-axis (longitudinal direction)
+# along 45 degree in x-y plane (angle alpha=45°):
 #a1 = Constant((1.0/sqrt(2), 1.0/sqrt(2), 0.0)) # direction of anisotropy along 45 degree in x-y plane # This means the material is stiffest along the 45 degree direction in the x-y plane
 
-A1 = outer(a1, a1)
-# Creates a second-order tensor representing the fiber orientation
+# Creates a second-order tensor representing the fibre orientation
 # A1 = a1 ⊗ a1
 # This tensor is used in anisotropic stress calculations
-
-#rho = 8.e3
-#g = 9.81
-
+A1 = outer(a1, a1)
 
 # Volume force/ heat source and prescribed tractions/ prescribed heat fluxes
 b = Constant((0.0, 0.0, 0.0)) # body force in N/m^3
 t_p = Constant((0.0, 0.0, 0.0)) # surface traction vector  in N/m^2 (Pa) 
-
-# Dirichlet boundary conditions
-#class Near(SubDomain):
-#	def __init__(self, point):
-#		self.p = point
-#		SubDomain.__init__(self)
-#	def inside(self, x, on_boundary):
-#		return (abs(x[0]-self.p[0]) < DOLFIN_EPS and \
-#						       abs(x[1]-self.p[1]) < DOLFIN_EPS and \
-#						       abs(x[2]-self.p[2]) < DOLFIN_EPS)
 
 class Origin(SubDomain):
 	def inside(self, x, on_boundary):
@@ -154,17 +145,7 @@ def sigma(u):
     	   + alpha*(dot(a1, dot(epsilon(u), a1))*Identity(dim) + tr(epsilon(u))*A1) \
 		   + 2.0*(mu_LT-mu_TT)*(dot(A1, epsilon(u))+dot(epsilon(u), A1)) \
 		   + beta*dot(a1, dot(epsilon(u), a1))*A1
-		   
-# Material parameters
-#E = 200.e9
-#nu = 0.3
-#mu    = E/(2.0*(1.0 + nu))
-#lmbda = E*nu/((1.0 + nu)*(1.0 - 2.0*nu))
-
-# Stress tensor (linear isotropic elasticity)
-#def sigma(u):
-#    return lmbda*tr(sym(grad(u)))*Identity(d) + 2.0*mu*sym(grad(u))
-		   
+		   		   
 # Weak form a==l
 a = inner(grad(delta_u), sigma(u))*dx # Bilinear form (internal virtual work)
 l = dot(b, delta_u)*dx + dot(t_p, delta_u)*ds(right) # Linear form (external virtual work) - body forces and surface tractions on the right boundary
